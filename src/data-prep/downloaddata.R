b3d68567c6d2f2174ddb4a1c7f1e410bcd11574a
Davejongenelen <d.c.r.jongenelen@tilburguniversity.edu>
2021-10-15 14:09:16 +0200

    readme file

--- src/data-prep/downloaddata.R
+++ src/data-prep/downloaddata.R
@@ -1,4 +1,4 @@
 # download venice data
-dir.create("../../data/")
+dir.create("data/")
 download.file('http://data.insideairbnb.com/switzerland/z%C3%BCrich/zurich/2021-09-29/data/listings.csv.gz',
-              '../../listings_zurich.csv.gz')
+              'data/listings_zurich.csv.gz')
