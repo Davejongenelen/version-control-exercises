11053c9f40f4d01c5e1639000019525fe9e41d0c
Davejongenelen <d.c.r.jongenelen@tilburguniversity.edu>
2021-10-19 18:49:07 +0200

    adjust

--- src/data-prep/downloaddata.R
+++ src/data-prep/downloaddata.R
@@ -1,4 +1,4 @@
-# download venice data
+# download zurich data
 dir.create("../../data/")
 download.file('http://data.insideairbnb.com/switzerland/z%C3%BCrich/zurich/2021-09-29/data/listings.csv.gz',
               '../../listings_zurich.csv.gz')
